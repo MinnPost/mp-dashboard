e84bbce067f519533d2bbfca1cd4ca1c448c6d6f
Alan Palazzolo <apalazzolo@minnpost.com>
2013-05-07 16:25:44 -0500

    Experimenting with saving metric history.

--- jobs/facebooklinkstats.rb
+++ jobs/facebooklinkstats.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 require 'net/http'
 require 'json'
- 
-# The url you are tracking
-sharedlink = URI::encode('minnpost.com')
- 
-SCHEDULER.every '10m', :first_in => '1s' do
+require 'sinatra/activerecord'
+require './models/metric_history'
+
+# Function to gets stats
+def get_fb_stats
   fbstat = []
+  sharedlink = URI::encode('minnpost.com')
  
   http = Net::HTTP.new('graph.facebook.com')
   response = http.request(Net::HTTP::Get.new("/fql?q=SELECT%20share_count,%20like_count,%20comment_count,%20total_count%20FROM%20link_stat%20WHERE%20url=%22#{sharedlink}%22"))
@@ -15,7 +16,24 @@ SCHEDULER.every '10m', :first_in => '1s' do
   fbcounts[0].each do |stat|
     fbstat << {:label=>stat[0], :value=>stat[1]}
   end
+  
+  { items: fbstat }
+end
+
+# Send data to dashboard
+SCHEDULER.every '2m', :first_in => '1s' do
+  send_event('fblinkstat', get_fb_stats)
  
-  send_event('fblinkstat', { items: fbstat })
- 
+end
+
+# Save data to db
+SCHEDULER.every '2m' do
+  @MH = MetricHistory.new({
+    metric: 'fblinkstat',
+    created: Time.now,
+    value: get_fb_stats
+  })
+  if @model.save
+    puts 'Save FB stats.'
+  end
 end
