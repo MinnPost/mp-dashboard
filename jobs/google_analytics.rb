392ad919b61c91fb7ecb3241d8f7d93e9cdd9ce4
Alan Palazzolo <apalazzolo@minnpost.com>
2013-05-20 16:25:59 -0500

    Adding in widget for pageviews per visit.

--- jobs/google_analytics.rb
+++ jobs/google_analytics.rb
@@ -18,52 +18,52 @@ require './models/metric'
 # https://developers.google.com/analytics/devguides/reporting/core/v3/limits-quotas
 ##
 
+# Global for Google API client, probably not the best.
+$gapi_client
+
 # GA profile IDs
-profile_id_minnpost_com = '6603264'
+ga_profile_id_minnpost_com = '6603264'
 
 # Connect
 def ga_connect
-  client = Google::APIClient.new(
-    :application_name => 'MP Dashboard',
-    :application_version => '0.0.1')
-    
-  key = OpenSSL::PKey::RSA.new(ENV['DASHING_GAPI_PRIVATE_KEY'], 'notasecret')
-  client.authorization = Signet::OAuth2::Client.new(
-    :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-    :audience => 'https://accounts.google.com/o/oauth2/token',
-    :scope => 'https://www.googleapis.com/auth/analytics.readonly',
-    :issuer => ENV['DASHING_GAPI_ISSUER'],
-    :signing_key => key)
-    
-  client.authorization.fetch_access_token!
-  
-  client
+  if $gapi_client == nil
+    $gapi_client = Google::APIClient.new(
+      :application_name => 'MP Dashboard',
+      :application_version => '0.0.1')
+      
+    key = OpenSSL::PKey::RSA.new(ENV['DASHING_GAPI_PRIVATE_KEY'], 'notasecret')
+    $gapi_client.authorization = Signet::OAuth2::Client.new(
+      :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+      :audience => 'https://accounts.google.com/o/oauth2/token',
+      :scope => 'https://www.googleapis.com/auth/analytics.readonly',
+      :issuer => ENV['DASHING_GAPI_ISSUER'],
+      :signing_key => key)
+      
+    $gapi_client.authorization.fetch_access_token!
+  end
 end
 
 # Send page views per visit to dashboard
-SCHEDULER.every '30s', :first_in => '1s' do
+SCHEDULER.every '1h', :first_in => '1s' do
   data = []
+  ga_connect()
+  analytics = $gapi_client.discovered_api('analytics', 'v3')
   
-  client = ga_connect()
-  analytics = client.discovered_api('analytics', 'v3')
-  startDate = DateTime.now.prev_month.strftime("%Y-%m-%d")
-  endDate = DateTime.now.strftime("%Y-%m-%d")
-  
-  visitCount = client.execute(:api_method => analytics.data.ga.get, :parameters => { 
-    'ids' => 'ga:' + profile_id_minnpost_com,
-    'start-date' => startDate,
-    'end-date' => endDate,
-    'dimensions' => 'ga:month,ga:week',
+  # Get last year per month
+  start_date = DateTime.now.prev_year.strftime('%Y-%m-%d')
+  end_date = DateTime.now.strftime('%Y-%m-01')
+  per_month = $gapi_client.execute(:api_method => analytics.data.ga.get, :parameters => { 
+    'ids' => 'ga:' + ga_profile_id_minnpost_com,
+    'start-date' => start_date,
+    'end-date' => end_date,
+    'dimensions' => 'ga:year,ga:month',
     'metrics' => 'ga:pageviewsPerVisit'
   })
   
-  puts visitCount.data.column_headers.map { |c|
-    c.name  
-  }.join("\t")
-  
-  visitCount.data.rows.each do |r|
-    puts r.join("\t"), "\n"
+  per_month.data.rows.each do |r|
+    data << { :x => DateTime.strptime(r[0] + r[1], '%Y%m').to_i , :y => r[2].to_f.round(3) }
   end
   
-  send_event('ga_pages_visit', :points => data )
+  puts data
+  send_event('google_analytics_page_views_per_visit', :points => data )
 end
