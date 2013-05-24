c6c8005f8dd3a156db23a5839c30846abb4d6172
Alan Palazzolo <apalazzolo@minnpost.com>
2013-05-24 13:02:05 -0500

    ADding visits per visitors per month and week.

--- jobs/google_analytics.rb
+++ jobs/google_analytics.rb
@@ -27,6 +27,8 @@ ga_profile_id_minnpost_com = '6603264'
 # Connect
 def ga_connect
   if $gapi_client == nil
+    puts 'Making Google Analytics client connector.'
+    
     $gapi_client = Google::APIClient.new(
       :application_name => 'MP Dashboard',
       :application_version => '0.0.1')
@@ -44,7 +46,7 @@ def ga_connect
 end
 
 # Send page views per visit to dashboard
-SCHEDULER.every '1h', :first_in => '1s' do
+SCHEDULER.every '1d', :first_in => '1s' do
   data = []
   ga_connect()
   analytics = $gapi_client.discovered_api('analytics', 'v3')
@@ -66,4 +68,57 @@ SCHEDULER.every '1h', :first_in => '1s' do
   end
   
   send_event('google_analytics_page_views_per_visit_mn', :points => data )
+end
+
+# Average visits per month
+SCHEDULER.every '1d', :first_in => '5s' do
+  data = []
+  ga_connect()
+  analytics = $gapi_client.discovered_api('analytics', 'v3')
+  
+  # Get last year per month
+  start_date = DateTime.now.prev_year.strftime('%Y-%m-%d')
+  end_date = DateTime.now.strftime('%Y-%m-01')
+  per_month = $gapi_client.execute(:api_method => analytics.data.ga.get, :parameters => { 
+    'ids' => 'ga:' + ga_profile_id_minnpost_com,
+    'start-date' => start_date,
+    'end-date' => end_date,
+    'dimensions' => 'ga:year,ga:month',
+    'metrics' => 'ga:visitors,ga:visits',
+    'filters' => 'ga:region==Minnesota'
+  })
+  
+  # ga:year 	ga:month 	ga:visitors 	ga:visits 
+  per_month.data.rows.each do |r|
+    data << { :x => DateTime.strptime(r[0] + r[1], '%Y%m').to_i , :y => (r[3].to_f / r[2].to_f).round(3) }
+  end
+  
+  send_event('google_analytics_visits_month_mn', :points => data )
+end
+
+# Average visits per week
+SCHEDULER.every '1d', :first_in => '5s' do
+  data = []
+  ga_connect()
+  analytics = $gapi_client.discovered_api('analytics', 'v3')
+  
+  # Get last year per week
+  start_date = DateTime.now.prev_year.strftime('%Y-%m-%d')
+  end_date = DateTime.now.strftime('%Y-%m-01')
+  per_week = $gapi_client.execute(:api_method => analytics.data.ga.get, :parameters => { 
+    'ids' => 'ga:' + ga_profile_id_minnpost_com,
+    'start-date' => start_date,
+    'end-date' => end_date,
+    'dimensions' => 'ga:year,ga:week',
+    'metrics' => 'ga:visitors,ga:visits',
+    'filters' => 'ga:region==Minnesota',
+    'max-results' => '1000'
+  })
+  
+  # ga:year 	ga:month 	ga:visitors 	ga:visits 
+  per_week.data.rows.each do |r|
+    data << { :x => DateTime.strptime(r[0] + r[1], '%Y%W').to_i , :y => (r[3].to_f / r[2].to_f).round(3) }
+  end
+  
+  send_event('google_analytics_visits_week_mn', :points => data )
 end
